0fa10bb4a16bb18e15b72571b720f7b36efcae80
Jelani Brandon <jelanibrandon@gmail.com>
2019-02-02 21:05:05 -0800

    Update directories listing

--- inc/lib-util-c/alarm_timer.h
+++ inc/lib-util-c/alarm_timer.h
@@ -1,7 +1,12 @@
 #ifndef TIMER_OBJECT_H
 #define TIMER_OBJECT_H
 
-#include <stdlib.h>
+#ifdef __cplusplus
+extern "C" {
+#else
+#include <stdbool.h>
+#endif
+
 #include "umock_c_prod.h"
 
 typedef struct ALARM_TIMER_INFO_TAG* ALARM_TIMER_HANDLE;
@@ -11,5 +16,9 @@ MOCKABLE_FUNCTION(, void, alarm_timer_destroy, ALARM_TIMER_HANDLE, handle);
 MOCKABLE_FUNCTION(, int, alarm_timer_start, ALARM_TIMER_HANDLE, handle, size_t, expire_sec);
 MOCKABLE_FUNCTION(, void, alarm_timer_reset, ALARM_TIMER_HANDLE, handle);
 MOCKABLE_FUNCTION(, bool, alarm_timer_is_expired, ALARM_TIMER_HANDLE, handle);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif // SCHEDULER
