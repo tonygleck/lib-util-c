8a22ae682ca116ea61b3e6fa00409e881e433bab
Jelani <46301723+tonygleck@users.noreply.github.com>
2020-07-27 19:07:23 -0700

    feature: adding atomics code to repo (#12)

---
+++ inc/lib-util-c/atomic_operations.h
@@ -0,0 +1,23 @@
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+#pragma once
+
+#ifdef __cplusplus
+extern "C" {
+#include <cstdint>
+#else
+#include <stdint.h>
+#endif
+
+#include "umock_c/umock_c_prod.h"
+
+MOCKABLE_FUNCTION(, long, atomic_increment, long*, value);
+MOCKABLE_FUNCTION(, int64_t, atomic_increment64, int64_t*, value);
+MOCKABLE_FUNCTION(, long, atomic_decrement, long*, value);
+MOCKABLE_FUNCTION(, int64_t, atomic_decrement64, int64_t*, value);
+MOCKABLE_FUNCTION(, long, atomic_add, long*, operand, long, value);
+MOCKABLE_FUNCTION(, long, atomic_subtract, long*, operand, long, value);
+
+#ifdef __cplusplus
+}
+#endif
