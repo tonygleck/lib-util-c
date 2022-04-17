5bc4c5d7ce37d32c56f7ff9cf338e4d1d786ab0f
Jelani Brandon <jelanibrandon@gmail.com>
2022-04-17 08:52:56 -0700

    chore: update file manager code

--- inc/lib-util-c/file_mgr.h
+++ inc/lib-util-c/file_mgr.h
@@ -12,9 +12,19 @@ extern "C" {
 #include "macro_utils/macro_utils.h"
 #include "umock_c/umock_c_prod.h"
 
+typedef enum file_mode_value_tag
+{
+    file_mode_read,
+    file_mode_write,
+    file_mode_append,
+    file_mode_read_write,
+    file_mode_write_new,
+    file_mode_read_append
+} file_mode_value;
+
 typedef struct FILE_MGR_INFO_TAG* FILE_MGR_HANDLE;
 
-MOCKABLE_FUNCTION(, FILE_MGR_HANDLE, file_mgr_open, const char*, filename, const char*, param);
+MOCKABLE_FUNCTION(, FILE_MGR_HANDLE, file_mgr_open, const char*, filename, file_mode_value, mode);
 MOCKABLE_FUNCTION(, void, file_mgr_close, FILE_MGR_HANDLE, handle);
 MOCKABLE_FUNCTION(, long, file_mgr_get_length, FILE_MGR_HANDLE, handle);
 MOCKABLE_FUNCTION(, size_t, file_mgr_read, FILE_MGR_HANDLE, handle, unsigned char*, buffer, size_t, read_len);
